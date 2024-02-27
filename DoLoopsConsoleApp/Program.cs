ab5589f58b703ddc1c6481f9a0e537d6c7323e94
Pierre Plourde <pierre@spartancsharp.net>
2024-02-27 11:26:37 -0500

    Create Code
    Create a Console Application that asks the user for their name. Welcome Tim as professor or anyone else as student. Do this until the user types "exit".

--- DoLoopsConsoleApp/Program.cs
+++ DoLoopsConsoleApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool done = false;
+
+do
+{
+	Console.Write("What is your first name (type 'exit' to quit): ");
+	string? firstName = Console.ReadLine();
+
+	if ( firstName?.ToLower() == "exit" )
+	{
+		done = true;
+	}
+	else if ( firstName?.ToLower() == "tim" )
+	{
+		Console.WriteLine($"Hi Professor {firstName}!");
+	}
+	else
+	{
+		Console.WriteLine($"Hi {firstName}!");
+	}
+} while ( !done );
